61fb09d4ccb6ef1a405db30ec9fcca738e88e769
crize <34015092+gurencrize@users.noreply.github.com>
2021-07-20 08:55:06 +0900

    Update ColorCodeCheck.js
    カラーコードの英字を全て大文字にしてから比較するようにした

--- ColorCodeCheck.js
+++ ColorCodeCheck.js
@@ -1,13 +1,20 @@
 let clearbutton = document.getElementById("ColorCodeCheck")
 ColorCodeCheck.onclick = function () {
     let val = document.getElementById("value").value
+    // TODO: アルファチャンネル付きのカラーコード対応もする？
+    // TODO: 背景色は変えられるようにした方が良い
     let regex = new RegExp("#[a-fA-f0-9]{6}", "g")
     let result = val.match(regex)
+    
+    // 配列の中身を全て大文字にする
+    result=result.map(function(value,index,array){
+        return value.toUpperCase()
+    })
 
-    // FIXME 現状大文字小文字を区別して重複の判定をしているけど、
-    // 重複の判定で大文字小文字は判定しなくていいかな…
-    // いっそのことアルファベットはすべて大文字にしても良いぐらい
+    // 重複している要素を削除
     result = new Set(result)
+
+    // この辺で関数を分ける
     let resulttext = ""
     if (result) {
         result.forEach(element => {
