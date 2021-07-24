2fae82655d3cfee46f53fef3a07e3209d824c3ef
crize <34015092+gurencrize@users.noreply.github.com>
2021-07-24 17:55:07 +0900

    Update ColorCodeCheck.js
    若干リファクタリング

--- ColorCodeCheck.js
+++ ColorCodeCheck.js
@@ -1,20 +1,7 @@
 let clearbutton = document.getElementById("ColorCodeCheck")
 ColorCodeCheck.onclick = function () {
     let val = document.getElementById("value").value
-    // TODO: アルファチャンネル付きのカラーコード対応もする？
-    // TODO: 背景色は変えられるようにした方が良い
-    let regex = new RegExp("#[a-fA-f0-9]{6}", "g")
-    let result = val.match(regex)
-    
-    // 配列の中身を全て大文字にする
-    result=result.map(function(value,index,array){
-        return value.toUpperCase()
-    })
-
-    // 重複している要素を削除
-    result = new Set(result)
-
-    // この辺で関数を分ける
+    let result=ColorCodeCheck(val)
     let resulttext = ""
     if (result) {
         result.forEach(element => {
@@ -23,5 +10,20 @@ ColorCodeCheck.onclick = function () {
     }
     text = document.getElementById("result")
     text.innerHTML = resulttext
-
 }
+
+function ColorCodeCheck(text){
+    // TODO: アルファチャンネル付きのカラーコード対応もする？
+    let regex = new RegExp("#[a-fA-f0-9]{6}", "g")
+    let result = text.match(regex)
+    
+    // 配列の中身を全て大文字にする
+    result = result.map(function(value,index,array){
+        return value.toUpperCase()
+    })
+
+    // 重複している要素を削除
+    result = new Set(result)
+    return result
+}
+// todo: 背景色は変えられるようにした方が良い
